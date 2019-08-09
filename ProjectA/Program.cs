e8eaccd76b5a0c332f0c057d548ad223b3397daa
Anand Gaurav <cs.anandgaurav@gmail.com>
2019-08-09 15:31:54 -0700

    First commit

---
+++ ProjectA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LockFileApplicability
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
